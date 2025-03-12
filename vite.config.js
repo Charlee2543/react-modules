85a0f7f6687079f9ba8be09bda52032f75b9fb55
Pongsakon <charlee2543@hotmail.com>
2025-03-12 15:25:45 +0700

    feat:edit component Accordion FAQ

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./tests/setup.js",
-  },
+	plugins: [react(), tsconfigPaths()],
+	test: {
+		globals: true,
+		environment: "jsdom",
+		setupFiles: "./tests/setup.js",
+	},
 });
